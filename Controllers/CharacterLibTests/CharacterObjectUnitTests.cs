48d06a6288554de4003d334c936f3442b5e6d9d4
Lynkfox <lynkfox@gmail.com>
2019-11-15 11:17:05 -0500

    Initial Commit

---
+++ Controllers/CharacterLibTests/CharacterObjectUnitTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CharacterLibTests
+{
+    [TestClass]
+    public class CharacterObjectUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
