2cc3c8d58b2ff5506bd52da5c5326935e2a89d5c
Lynkfox <lynkfox@gmail.com>
2019-11-16 20:06:43 -0500

    TalentStoresCostToInvest() Green

---
+++ Controllers/CharacterLib/Talents.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterLib
+{
+    public class Talents
+    {
+        public int Cost { get; set; } = 5;
+    }
+}
---
+++ Controllers/CharacterLibTests/TalentsTests.cs
@@ -0,0 +1,20 @@
+﻿using CharacterLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CharacterLibTests
+{
+    [TestClass]
+    public class TalentTests
+    {
+        [TestMethod]
+        public void TalentStoresCostToInvest()
+        {
+            var talent = new Talents();
+            int expectedCost = 5;
+
+            talent.Cost = 5;
+
+            Assert.AreEqual(expectedCost, talent.Cost);
+        }
+    }
+}
