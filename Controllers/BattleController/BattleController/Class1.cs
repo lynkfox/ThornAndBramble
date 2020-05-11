7782d4c84aa4f8e12b809b22f71a359f3d13d270
Lynkfox <lynkfox@gmail.com>
2020-05-11 13:29:32 -0400

    BattlefieldCanHaveMonstersAddedToIt Green

--- Controllers/BattleController/BattleController/Class1.cs
+++ Controllers/BattleController/BattleController/Class1.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using CharacterLib;
 
 namespace BattleController
 {
-    public class Class1
+    public class Battlefield
     {
+        public int MonsterCount { get; set; }
+
+        private List<MonsterCharacter> MonstersOnField = new List<MonsterCharacter>();
+        public void SpawnMonster(MonsterCharacter genericMonster)
+        {
+            MonstersOnField.Add(genericMonster);
+            MonsterCount = MonstersOnField.Count;
+        }
     }
 }
--- Controllers/BattleController/BattleControllerUnitTests/BattleControllerUnitTests.cs
+++ Controllers/BattleController/BattleControllerUnitTests/BattleControllerUnitTests.cs
@@ -1,13 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BattleController;
+using CharacterLib;
 
 namespace BattleControllerUnitTests
 {
     [TestClass]
     public class BattleControllerUnitTests
     {
+        Battlefield testBattle = new Battlefield();
+        MonsterCharacter genericMonster = new MonsterCharacter();
+
         [TestMethod]
-        public void TestMethod1()
+        public void BattlefieldCanHaveMonstersAddedToIt()
+
         {
+            testBattle.SpawnMonster(genericMonster);
+            int expectedMonsterCount = 1;
+
+            Assert.AreEqual(expectedMonsterCount, testBattle.MonsterCount);
         }
     }
 }
