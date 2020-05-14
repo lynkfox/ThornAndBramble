bb9bb0c72db1abd83ed461aa865fb609fa52548d
Lynkfox <lynkfox@gmail.com>
2020-05-14 16:17:10 -0400

    Starting on Reader for XML files for profiles

---
+++ Models/Characters/CharacterLib/ProfileReader.cs
@@ -0,0 +1,18 @@
+﻿using CharacterLib.Structures;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+
+namespace CharacterLib
+{
+    public static class ProfileReader
+    {
+        public static Talent ReadTalent(string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
